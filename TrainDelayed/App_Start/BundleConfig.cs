fe12896eed0fb019714fec649f1e29dbf251263f
Michael Pritchard <blueghostuk@gmail.com>
2013-03-27 21:57:17 +0000

    add base

---
+++ TrainDelayed/App_Start/BundleConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Web.Optimization;
+
+namespace TrainDelayed
+{
+    public static class BundleConfig
+    {
+        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/knockout").Include(
+                "~/Scripts/knockout-{version}.js",
+                "~/Scripts/knockout.mapping-latest.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                "~/Scripts/bootstrap*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/moment").Include(
+                "~/Scripts/moment.js",
+                "~/Scripts/moment-datepicker.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                "~/Content/bootstrap.css",
+                "~/Content/bootstrap-responsive.css"));
+        }
+    }
+}
---
+++ TrainDelayed/Global.asax.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Web.Optimization;
+
+namespace TrainDelayed
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+    }
+}
